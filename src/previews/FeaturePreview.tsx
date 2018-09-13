a718151d0c72da1a4018d6ccb0964815f007b004
barnsey-tran <barnseyog@gmail.com>
2018-09-13 12:41:11 +0930

    Pass the right value to component

--- src/previews/FeaturePreview.tsx
+++ src/previews/FeaturePreview.tsx
@@ -5,7 +5,7 @@ export default (props: any) => (
   <FeatureComponent
     item = {{
       title: props.entry.getIn(["data", "title"]),
-      contents: props.entry.getIn("data", "body"),
+      contents: props.entry.getIn(["data", "body"]),
       image: props.entry.getIn(["data", "image"]),
       description: props.entry.getIn(["data", "description"]),
       imagePosition: props.entry.getIn(["data", "imagePosition"]),
