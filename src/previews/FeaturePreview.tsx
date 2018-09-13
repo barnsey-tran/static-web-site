01fa947ae774da75ceea13a1123af417545e4acd
barnsey-tran <barnseyog@gmail.com>
2018-09-13 12:38:04 +0930

    Fixed contents of component

--- src/previews/FeaturePreview.tsx
+++ src/previews/FeaturePreview.tsx
@@ -5,8 +5,9 @@ export default (props: any) => (
   <FeatureComponent
     item = {{
       title: props.entry.getIn(["data", "title"]),
-      contents: props.widgetFor("body"),
+      contents: props.entry.getIn("data", "body"),
       image: props.entry.getIn(["data", "image"]),
+      description: props.entry.getIn(["data", "description"]),
       imagePosition: props.entry.getIn(["data", "imagePosition"]),
     }}
   />
