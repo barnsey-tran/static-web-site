c370f1cda995f5f2177fc498c7dad5e37e4cd5e7
barnsey-tran <barnseyog@gmail.com>
2018-09-14 10:16:25 +0930

    Fixed preview template error

--- src/previews/HomePreviewTemplate.tsx
+++ src/previews/HomePreviewTemplate.tsx
@@ -4,25 +4,28 @@ import NewsComponent from "../home/NewsComponent";
 
 export default (props: any) => {
   let componentType: string = props.entry.getIn(["data", "component"]);
-  let returnComponent: any = componentType === "feature"
-                  ? <FeatureComponent
-                      item = {{
-                        title: props.entry.getIn(["data", "title"]),
-                        contents: props.entry.getIn(["data", "body"]),
-                        image: props.entry.getIn(["data", "image"]),
-                        description: props.entry.getIn(["data", "description"]),
-                        imagePosition: props.entry.getIn(["data", "imagePosition"]),
-                      }}
-                    />
-                  : <NewsComponent
-                      item={{
-                        title: props.entry.getIn(["data", "title"]),
-                        contents: props.entry.getIn(["data", "body"]),
-                        image: props.entry.getIn(["data", "image"]),
-                        description: props.entry.getIn(["data", "description"]),
-                      }}
-                    />;
   return (
-    { returnComponent }
+    <div>
+      {
+        componentType === "feature"
+        ? <FeatureComponent
+            item = {{
+              title: props.entry.getIn(["data", "title"]),
+              contents: props.entry.getIn(["data", "body"]),
+              image: props.entry.getIn(["data", "image"]),
+              description: props.entry.getIn(["data", "description"]),
+              imagePosition: props.entry.getIn(["data", "imagePosition"]),
+            }}
+          />
+        : <NewsComponent
+            item={{
+              title: props.entry.getIn(["data", "title"]),
+              contents: props.entry.getIn(["data", "body"]),
+              image: props.entry.getIn(["data", "image"]),
+              description: props.entry.getIn(["data", "description"]),
+            }}
+          />
+       }
+    </div>
   );
 };
